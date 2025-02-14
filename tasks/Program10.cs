014ed6a1126ae2cbcc2ec9c4a4a77e5f303b8e7a
Arman <arman2004vardanyan@mail.ru>
2025-02-14 23:00:24 +0400

    tasks

---
+++ tasks/Program10.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] arr = new int[3][];
+        arr[0] = new int[] { 2, 3, 4 };
+        arr[1] = new int[] { 3, 5 };
+        arr[2] = new int[] { 4, 1, 8 };
+
+        for(int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"\nRow {i}:\n");
+
+            for(int j = 0; j < arr[i].Length; j++)
+            {
+                Console.WriteLine($"arr[{i}][{j}]: {arr[i][j]}");
+            }
+        }
+    }
+}
---
+++ tasks/Program6.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double height;
+
+        Console.WriteLine("Enter height:");
+
+        height = double.Parse(Console.ReadLine());
+
+        double weight;
+
+        Console.WriteLine("Enter weight:");
+
+        weight = double.Parse(Console.ReadLine());
+
+        double BMI = weight / (height * height);
+
+        Console.WriteLine($"BMI: {BMI}. " +
+            (
+                BMI < 18.5 ? "Underweight" : 
+                BMI <= 24.9 ? "Normal weight" : 
+                BMI <= 29.9 ? "Overweight" :
+                "Obese"
+            )
+        );
+    }
+}
