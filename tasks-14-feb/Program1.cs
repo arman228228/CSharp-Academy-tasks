b30702239ab13a1176258470caf319fd8b383973
Arman <arman2004vardanyan@mail.ru>
2025-02-15 04:45:27 +0400

    tasks

---
+++ tasks-14-feb/Program1.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int fibonacc_n_1 = 0, fibonacc_n_2 = 1;
+
+        Console.Write($"\nFibonacci numbers:\n{fibonacc_n_1}");
+
+        for (int i = 1, next; i < n; i++) 
+        {
+            Console.Write(" " + fibonacc_n_2);
+
+            next = fibonacc_n_1 + fibonacc_n_2;
+            
+            fibonacc_n_1 = fibonacc_n_2;
+            fibonacc_n_2 = next;
+        }
+    }
+}
---
+++ tasks-14-feb/Program2.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()), res = 0;
+
+        while(n > 0)
+        {
+            res = res * 10 + n % 10;
+            n /= 10;
+        }
+
+        Console.WriteLine(res);
+    }
+}
