c7a90a783e037d6d2792e9beff98a63a2c610a7b
Arman <arman2004vardanyan@mail.ru>
2025-02-24 00:26:25 +0400

    new tasks

---
+++ tasks-21-feb/Program6.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter numbers:");
+
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Enter {i} index number:");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Sum: " + CalculateSum.GetSum(numbers));
+    }
+}
+
+class CalculateSum
+{
+    static public int GetSum(params int[] numbers)
+    {
+        int sum = 0;
+
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
---
+++ tasks-21-feb/Program8.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter n:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci: " + FibonacciCalculations.GetNumberIndex(n));
+    }
+}
+
+class FibonacciCalculations
+{
+    public static int GetNumberIndex(int n) 
+    {
+        if(n < 2) return n;
+
+        return GetNumberIndex(n - 1) + GetNumberIndex(n - 2);
+    }
+}
