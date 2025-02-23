da9a99ef42ba97d5de69f65abb60361fe50740a7
Arman <arman2004vardanyan@mail.ru>
2025-02-24 00:27:32 +0400

    fix

--- tasks-19-feb/Program9PartialSecond.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp4;
-
-partial class Character
-{
-    public void ShowCharacterInfo()
-    {
-        Console.WriteLine($"Character name: {characterName}, level: {level}");
-    }
-}
