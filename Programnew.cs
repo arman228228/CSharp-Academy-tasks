49f33642e3df84cbcb93398188401aecfec6077a
Arman <98423161+arman228228@users.noreply.github.com>
2025-03-01 17:57:41 +0400

    Add files via upload

---
+++ Programnew.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WaterTank a = new WaterTank(100, 5);
+        WaterTank b = new WaterTank(150, 5);
+
+        a = a + b;
+        Console.WriteLine(a);
+    }
+}
+
+class WaterTank
+{
+    double Capacity;
+    double CurrentLevel;
+
+    public WaterTank(double capacity, double currentLevel)
+    {
+        Capacity = capacity;
+        CurrentLevel = currentLevel;
+    }
+
+    public static WaterTank operator +(WaterTank first, WaterTank second)
+    {
+        double newWaterLevel = first.CurrentLevel + second.CurrentLevel;
+        if(newWaterLevel > first.Capacity)
+        {
+            Console.WriteLine($"First water tank limit reached. ({first.Capacity} limit)");
+            return null;
+        }
+
+        return new WaterTank(first.Capacity, newWaterLevel);
+    }
+
+    public static WaterTank operator -(WaterTank tank, double water)
+    {
+        double newWaterLevel = tank.CurrentLevel - water;
+        if(newWaterLevel < 0)
+        {
+            Console.WriteLine($"Water tank do not go below 0");
+            return null;
+        }
+
+        return new WaterTank(tank.Capacity, newWaterLevel);
+    }
+
+    public override string ToString()
+    {
+        return $"Capacity: {Capacity}, Current level: {CurrentLevel}";
+    }
+}
---
+++ Programnew2.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InkReservoir a = new InkReservoir("red", 5);
+        InkReservoir b = new InkReservoir("red", 2);
+
+        // a = a + b;
+        Console.WriteLine(a.Equals(b) ? "true" : "false");
+
+        // Console.WriteLine("-------");
+
+        // InkReservoir c = new InkReservoir("red", 3);
+        // c = a + a;
+        // Console.WriteLine(c);
+    }
+}
+
+class InkReservoir
+{
+    string Color;
+    double InkAmount;
+
+    public InkReservoir(string color, double inkAmount)
+    {
+        Color = color;
+        InkAmount = inkAmount;
+    }
+
+    public static InkReservoir operator +(InkReservoir a, InkReservoir b)
+    {
+        if(a.Color != b.Color)
+        {
+            Console.WriteLine("Incompatible colors");
+            return a;
+        }
+
+        return new InkReservoir(a.Color, a.InkAmount + b.InkAmount);
+    }
+
+    public static InkReservoir operator -(InkReservoir reservoir, double amount)
+    {
+        double result = reservoir.InkAmount - amount;
+
+        if(result < 0)
+        {
+            Console.WriteLine("Not enough ink");
+            return reservoir;
+        }
+
+        return new InkReservoir(reservoir.Color, result);
+    }
+
+    public override string ToString()
+    {
+        return $"Color: {Color}, Amount: {InkAmount}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is InkReservoir reservoir ? Color == reservoir.Color : false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Color, InkAmount);
+    }
+}
