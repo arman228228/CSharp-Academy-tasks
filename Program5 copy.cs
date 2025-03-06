3f936a0c539d93f530075216e59ad91c588beec6
Arman <98423161+arman228228@users.noreply.github.com>
2025-03-06 18:34:03 +0400

    Add files via upload

---
+++ Program5 copy.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Students students = new Students();
+        
+        Console.WriteLine("Ann grade: " + students["Ann"]);
+    }
+}
+
+class Students
+{
+    private string[] subjects = {"Bob", "Ann", "David"};
+    private int[] grades = {78, 90, 50};
+
+    public int this[string subject]
+    {
+        get
+        {
+            for(int i = 0; i < subjects.Length; i++)
+            {
+                if(subjects[i] != subject)
+                {
+                    continue;
+                }
+
+                return grades[i];
+            }
+
+            return -1;
+        }
+    }
+}
