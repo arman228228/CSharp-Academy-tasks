5d0d56183f9ebf5a480b324741141f97df18792b
Arman <98423161+arman228228@users.noreply.github.com>
2025-03-06 17:26:41 +0400

    Add files via upload

---
+++ ProgramDateTimeFormat.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TimePeriod formattedTime = new TimePeriod();
+        formattedTime.SetSeconds(3700);
+
+        Console.WriteLine(formattedTime.FormattedTime);
+    }
+}
+
+class TimePeriod
+{
+    int TotalSeconds { get; set; }
+
+    public void SetSeconds(in int seconds)
+    {
+        TotalSeconds = seconds;
+    }
+
+    public string FormattedTime 
+    { 
+        get
+        {
+            int hours = TotalSeconds / 3600,
+                minutes = (TotalSeconds % 3600) / 60,
+                seconds = TotalSeconds % 60
+            ;
+
+            return $"{hours.ToString("00")}:{minutes.ToString("00")}:{seconds.ToString("00")}";
+        }
+    }
+}
