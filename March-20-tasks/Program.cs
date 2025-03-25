ccd06ad2f6cb97b7fc9e323aab73d4a8e5f58094
Arman <arman2004vardanyan@mail.ru>
2025-03-26 01:14:30 +0400

    New tasks

---
+++ March-20-tasks/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace ConsoleApp13;
+
+readonly struct DefaultWords
+{
+    static readonly string[] EnglishWords =
+    [
+        "Apple", "Banana", "Orange", "Mango", "Pineapple", "Grape", "Strawberry", "Cherry", "Watermelon", "Peach"
+    ];
+    
+    static readonly string[] RussianWords =
+    [
+        "Яблоко", "Банан", "Апельсин", "Манго", "Ананас", "Виноград", "Клубника", "Вишня", "Арбуз", "Персик"
+    ];
+    
+    static readonly string[] SpanishWords =
+    [
+        "Manzana", "Plátano", "Naranja", "Mango", "Piña", "Uva", "Fresa", "Cereza", "Sandía", "Melocotón"
+    ];
+    
+    static readonly string[] FrenchWords =
+    [
+        "Pomme", "Banane", "Orange", "Mangue", "Ananas", "Raisin", "Fraise", "Cerise", "Pastèque", "Pêche"
+    ];
+
+    public static string GetRussianWord(string text)
+    {
+        for (int word = 0; word < EnglishWords.Length; word++)
+        {
+            if (DefaultWords.EnglishWords[word] == text)
+            {
+                return RussianWords[word];
+            }
+        }
+
+        return "none";
+    }
+    
+    public static string GetSpanishWord(string text)
+    {
+        for (int word = 0; word < EnglishWords.Length; word++)
+        {
+            if (DefaultWords.EnglishWords[word] == text)
+            {
+                return SpanishWords[word];
+            }
+        }
+
+        return "none";
+    }
+    
+    public static string GetFrenchWord(string text)
+    {
+        for (int word = 0; word < EnglishWords.Length; word++)
+        {
+            if (DefaultWords.EnglishWords[word] == text)
+            {
+                return FrenchWords[word];
+            }
+        }
+
+        return "none";
+    }
+}
+
+abstract class Translator
+{
+    public abstract string Translate(string text);
+}
+
+class Russian : Translator
+{
+    public override string Translate(string text)
+    {
+        return DefaultWords.GetRussianWord(text);
+    }
+}
+
+class Spanish : Translator
+{
+    public override string Translate(string text)
+    {
+        return DefaultWords.GetSpanishWord(text);
+    }
+}
+
+class French : Translator
+{
+    public override string Translate(string text)
+    {
+        return DefaultWords.GetFrenchWord(text);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        Translator[] words = new Translator[3];
+        
+        words[0] = new Spanish();
+        words[1] = new French();
+        words[2] = new Russian();
+        
+        foreach(Translator t in words)
+        {
+            Console.WriteLine(t.Translate("Apple"));
+        }
+    }
+}
---
+++ March-20-tasks/Program2.cs
@@ -0,0 +1,70 @@
+﻿namespace ConsoleApp14;
+
+abstract class Shape
+{
+    public abstract int Surface();
+    public abstract void Draw();
+
+    public void Print()
+    {
+        Console.WriteLine($"{this.GetType().Name}, S = {Surface()}");
+        Draw();
+    }
+}
+
+class Square(int value) : Shape
+{
+    public override int Surface()
+    {
+        return value * value;
+    }
+    
+    public override void Draw()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write("+ ");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
+
+class Rectangle(int firstValue, int secondValue) : Shape
+{
+    public override int Surface()
+    {
+        return firstValue * secondValue;
+    }
+    
+    public override void Draw()
+    {
+        for (int i = 0; i < firstValue; i++)
+        {
+            for (int j = 0; j < secondValue; j++)
+            {
+                Console.Write("+ ");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Square(4);
+        shapes[1] = new Rectangle(4, 6);
+        
+        foreach(Shape s in shapes)
+        {
+            s.Print();
+        }
+    }
+}
